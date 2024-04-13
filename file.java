cf30c91d5f6b1c6b8dfc0edf1c9df1f263a1c304
BYSANI SAI AMARNATH <92385390+bysanisaiamarnath960@users.noreply.github.com>
2024-04-13 09:50:06 +0530

    first commit

---
+++ file.java
@@ -0,0 +1,25 @@
+ // HelloWorldServlet.java
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<p>This is a simple Java web application deployed on Tomcat.</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
